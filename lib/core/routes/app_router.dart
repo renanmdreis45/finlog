e2e89f31084372c741a0e1ed68bfd74393ca34a2
renanmdreis45 <renanmdreis@alu.ufc.br>
2025-06-09 23:24:05 -0300

    feat: init app router

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,25 @@
+import 'package:finlog/core/routes/routes_name.dart';
+import 'package:finlog/modules/home/presentation/home_view.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteNames.home:
+        return MaterialPageRoute(builder: (_) => const HomeView());
+      default:
+        return _errorRoute(settings.name);
+    }
+  }
+
+  static Route<dynamic> _errorRoute(String? routeName) {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+        body: Center(
+          child: Text('Route "$routeName" not found'),
+        ),
+      ),
+    );
+  }
+}
