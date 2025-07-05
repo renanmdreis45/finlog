fa288ca38e174ff6b35f67360dc434f57b14528e
renanmdreis45 <renanmdreis@alu.ufc.br>
2025-07-05 16:10:27 -0300

    feat: domain - add profile model

---
+++ lib/modules/home/domain/profile.dart
@@ -0,0 +1,11 @@
+class Profile {
+  final String name;
+  final String photo;
+  final double balance;
+
+  const Profile({
+    required this.name,
+    required this.photo,
+    required this.balance,
+  });
+}
